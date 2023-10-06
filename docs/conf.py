eae18699b7289fdcae032008b12c2aceb698d0be
straightCrimpin <straightcrimpin89@gmail.com>
2023-10-06 10:47:31 -0500

    Change index.rst to Readme.rst

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = 'Readme'
 
 # -- Options for HTML output -------------------------------------------------
 
